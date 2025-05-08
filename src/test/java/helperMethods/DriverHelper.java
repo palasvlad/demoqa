d8f057ce1189509a91b442eb36bafe8119eda2ba
vpalasanu <vlad.palasanu@gmail.com>
2025-05-08 10:00:30 +0300

    Test lock branch

--- src/test/java/helperMethods/DriverHelper.java
+++ src/test/java/helperMethods/DriverHelper.java
@@ -7,7 +7,7 @@ import java.net.URL;
 
 public class DriverHelper {
 
-    public static WebDriver setupWebDriver(String testURL){
+    public static WebDriver setupWebDriver(String testURL) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(testURL);
