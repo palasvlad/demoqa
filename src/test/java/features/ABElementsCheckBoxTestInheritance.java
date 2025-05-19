1e7d4baa9a4bc9e434ee29411fa9f1d2197fa7fa
vpalasanu <vlad.palasanu@gmail.com>
2025-05-19 11:27:17 +0300

    small oop update

---
+++ src/test/java/features/ABElementsCheckBoxTestInheritance.java
@@ -0,0 +1,56 @@
+package features;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import helperMethods.SetupCommon;
+
+public class ABElementsCheckBoxTestInheritance extends SetupCommon {
+
+    ABElementsCheckBoxTestInheritance (){
+        super();
+    }
+    @Test
+    public void testelementsCheckBox() {
+
+        window.scroll(0,500);
+
+        WebElement elementsMenu = driver.findElement(By.xpath("//h5[text()='Elements']"));
+        elementsMenu.click();
+
+        WebElement textBoxMenuButton = driver.findElement(By.xpath("//span[text()='Check Box']"));
+        textBoxMenuButton.click();
+
+        window.scroll(0,200);
+        WebElement checkboxSpan = driver.findElement(By.className("rct-checkbox"));
+        checkboxSpan.click();
+
+        WebElement selectedResult = driver.findElement(By.id("result"));
+        Assert.assertTrue(selectedResult.getText().contains("You have selected"));
+        Assert.assertTrue(selectedResult.getText().contains("home"));
+
+        WebElement collapseButton = driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/span/button"));
+        collapseButton.click();
+
+        WebElement collapseButtonDesktop = driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[1]/span/button"));
+        collapseButtonDesktop.click();
+
+        WebElement collapseButtonDocuments = driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[2]/span/button"));
+        collapseButtonDocuments.click();
+
+        window.scroll(0,200);
+        WebElement collapseButtonDownloads = driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[3]/span/button"));
+        collapseButtonDownloads.click();
+
+        WebElement collapseButtonDocuWorkspace = driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[2]/ol/li[1]/span/button"));
+        collapseButtonDocuWorkspace.click();
+
+        WebElement collapseButtonDocuOffice = driver.findElement(By.xpath("//*[@id=\"tree-node\"]/ol/li/ol/li[2]/ol/li[2]/span/button"));
+        collapseButtonDocuOffice.click();
+
+        //ceva validari poate ar mai merge pe aici since there's almost nada
+
+
+    }
+}
---
+++ src/test/java/helperMethods/SetupCommon.java
@@ -0,0 +1,25 @@
+package helperMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class SetupCommon {
+    public WebDriver driver;
+    public Window window;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        window = new Window(driver);
+    }
+
+    @AfterMethod
+    public void clearBrowser(){
+        driver.quit();
+    }
+
+}
